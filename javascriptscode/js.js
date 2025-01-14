aa8619baaa22c71968d43941f777ea16d485e15b
MariaKrasnaya <mariakrasny@yandex.ru>
2025-01-14 10:32:14 +0300

    c

--- javascriptscode/js.js
+++ javascriptscode/js.js
@@ -1,6 +1,69 @@
 document.addEventListener ("DOMContentLoaded" , function () {
 
-alert ("Привет, мир!");
+// alert ("Привет, мир!");
+// alert ("Привет")
+// alert ("Мир")
+
+// alert("3+1+2");
+
+// let message = "Hi" // let это переменная коробочка, message это название коробочки
+// alert(message) // alert всплывающее окно
+
+// let name = "Иван";
+// alert(`Привет, ${name}!`);
+
+// let isG = 4>1;
+// alert(isG);
+
+// let ter = 7>12;
+// console.log(ter);
+
+// let has=true;
+// let isL=false;
+
+// let can=has && isL;
+// console.log(can);
+
+// let temperature = 25;
+// let isHot = temperature > 30 ? "Горячо": "Не горячо";
+// alert(isHot);
+// console.log(isHot); // это вывод в консоль, а когда пишем алерт - это вывод на страницу в виде всплывающего окна
+
+// let amount = -15;
+// let positive = amount > 0 ? "Положительное" : "Отрицательное или ноль";
+// alert(positive)
+
+// let age = prompt("Сколько тебе лет?", 100);
+// alert(`Тебе ${age} лет!`);
+
+
+
+// let l = prompt("Введите ваш возраст:", 40);
+// let am = parseInt(l) + 1; // функция которая округляет
+// console.log("В следующем году вам будет" + am + "лет");
+
+
+
+
+// let usA = prompt("Вы согласны с условиями использования? (да/нет)");
+
+// if (usA.toLowerCase()=== "да") {
+// console.log("Спасибо за согласие!");
+// }
+
+// else if (usA === null) {
+// console.log("Операция отменена");
+// }
+
+// else {
+// console.log("Вы отказались от условий использования.")
+// }
+
+
+let isBoss = confirm("Ты здесь главный?");// вопрос, а prompt это запрос, где надо ввести данные
+alert(isBoss);
+
+
 
 
 });
